0b5e0d713a6c3237779867a3579a56c899bf5bae
Chakri Chikkaala <chakridol143@gmail.com>
2025-11-17 17:34:47 +0530

    server file vercel domain changed

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:4200',        
-    'https://ecom-test-nine-nu.vercel.app/'
+    'https://ecom-test-nine-nu.vercel.app'
   ],
   credentials: true
 }));
