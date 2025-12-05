32f347852c2c7c9c4ab2190eb8d5c4039d5d8531
Chakri Chikkaala <chakridol143@gmail.com>
2025-12-05 12:53:22 +0530

    .env file added google auth del

--- src/routers/google.router.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { googleAuth } from "../controller/google.controller";
-
-const router = Router();
-
-router.post("/google", googleAuth);
-
-export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import path, { join } from 'path';
-import googleRouter from "./routers/google.router";
 import categoryRoutes from './routers/category.routes';
 import productRoutes from './routers/product.routes';
 import loginRoutes from './routers/login.routes';
@@ -33,7 +32,6 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/checkout", checkoutRoutes);
 app.use('/api/admin/products', adminProducts);
 app.use('/api/admin/categories', adminCategories);
-app.use("/auth", googleRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
